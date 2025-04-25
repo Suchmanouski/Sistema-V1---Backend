0733cde6c28f40707d147b191d53ae2f827fdb7f
Gerrard Suchmanouski <gerrardsuchmanouskisilva@gmail.com>
2025-04-25 01:50:35 -0300

    Corrige importação e estrutura da rota de login e logs

--- Rotas/Login.js
+++ Rotas/Login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { listarLogs } = require('../Controles/loginC'); // Ajuste para listar logs
+const { listarLogs } = require('../Controles/LoginC'); // Ajuste para listar logs
 
 // Rota para Listar Logs de Atividades
 router.get('/', async (req, res) => {
