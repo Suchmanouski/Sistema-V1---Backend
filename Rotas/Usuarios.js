a74d367e4e040621e39a358aced32fdf6e821be3
Gerrard Suchmanouski <gerrardsuchmanouskisilva@gmail.com>
2025-04-25 01:33:40 -0300

    Corrige importação e estrutura da rota de login e logs

--- Rotas/Usuarios.js
+++ Rotas/Usuarios.js
@@ -1,7 +1,7 @@
+const { autenticarToken, verificarAdmin } = require('../middleware/autenticacao');
 const express = require('express');
 const router = express.Router();
 const { loginUsuario, cadastrarUsuario } = require('../Controles/usuariosC');
-const { autenticarToken, verificarAdmin } = require('../middleware/autenticacao');
 
 router.post('/login', loginUsuario);
 router.post('/usuarios', autenticarToken, verificarAdmin, cadastrarUsuario);
